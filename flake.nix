3857cc0694b93bfa4b482581cb81ce078431dfeb
Jiho Song <jihojiho2003@gmail.com>
2025-10-06 11:29:23 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Adguard Home Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            podman
+            podman-compose
+          ];
+
+          shellHook = ''
+            echo "AdGuard Home development shell"
+          '';
+        };
+      }
+    );
+}
